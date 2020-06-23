7582b6e39c3e66c3139928b0ffe599d1afdd136f
adityaag121 <54991705+adityaag121@users.noreply.github.com>
2020-06-24 00:16:13 +0530

    Updates

---
+++ precache-manifest.78af7db901ea70560650ee8fa3390b14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6f3cbdc7d7637c75674b8aae268c6b",
+    "url": "/assgn3-react/index.html"
+  },
+  {
+    "revision": "b18e42ad8ee60d5de4fc",
+    "url": "/assgn3-react/static/css/main.5ce743ae.chunk.css"
+  },
+  {
+    "revision": "126931b9a6887865ef28",
+    "url": "/assgn3-react/static/js/2.f8bb7958.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/assgn3-react/static/js/2.f8bb7958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18e42ad8ee60d5de4fc",
+    "url": "/assgn3-react/static/js/main.477a02fb.chunk.js"
+  },
+  {
+    "revision": "95306af007fe10094daa",
+    "url": "/assgn3-react/static/js/runtime-main.c26681cb.js"
+  },
+  {
+    "revision": "0f91b36c18b591c77546675237019038",
+    "url": "/assgn3-react/static/media/background.0f91b36c.jpg"
+  },
+  {
+    "revision": "ff82b6972f31f96be8c40bda4b828fe6",
+    "url": "/assgn3-react/static/media/logo512.ff82b697.png"
+  }
+]);
